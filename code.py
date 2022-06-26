9076550db4d2af513e6dcab0de2ab3d8e089f809
ONKJUNUA <jaseko2002@gmail.com>
2022-06-26 22:57:18 +0200

    endless mode

--- code.py
+++ code.py
@@ -326,6 +326,269 @@ class Game:
         self.display_score()
         self.next_level()
 
+class Game2:
+    def __init__(self):
+        player_sprite = Player((screen_width/2, screen_height - 10), screen_width)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
+        self.killable = False
+        self.level = 0
+        self.lives = 6
+        self.dmg = 2
+        self.speed_charge = 5
+        self.bullet_charge = 5
+        self.live_icon = pygame.image.load('graphics/player.png').convert_alpha()
+        self.score = 0
+        self.font = pygame.font.Font('font/pixel.ttf', 25)
+        self.level_font = pygame.font.Font('font/pixel.ttf', 35)
+        self.title_font = pygame.font.Font('font/pixel.ttf', 50)
+        
+        self.alien_cooldown = 1000
+        self.shape = obstacles.shape
+        self.block_size = 3
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num *(screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 12, y_start = 770)
+
+        self.aliens = pygame.sprite.Group()
+        self.alien_direction = 1
+        self.alien_lasers = pygame.sprite.Group()
+
+        self.extra = pygame.sprite.Group()
+        self.extra_spawn_time = randint (1500, 1500)
+
+        self.heart = pygame.sprite.Group()
+        self.shield = pygame.sprite.Group()
+        self.laser_speed = pygame.sprite.Group()
+        self.bullet = pygame.sprite.Group()
+        self.damage = pygame.sprite.Group()
+
+    def death(self):
+        lose2()
+
+    def create_obstacle(self, x_start, y_start,offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index,col in enumerate(row):
+                if col == 'x':
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacles.Block(self.block_size,(120,120,120),x,y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacles(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
+
+    def alien_setup(self, rows, cols, x_distance = 85, y_distance = 80, x_offset = 27, y_offset = 100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+
+                if self.level <= 4:                
+                    if row_index <= 1: alien_sprite = Alien(str(randint(5,6)), x, y)
+                    elif 2 <= row_index <= 3: alien_sprite = Alien(str(randint(3,4)), x, y)
+                    else: alien_sprite = Alien(str(randint(1,2)), x, y)
+                    self.aliens.add(alien_sprite)
+                elif 5 <= self.level <= 8:                
+                    if row_index <= 1: alien_sprite = Alien(str(randint(7,9)), x, y)
+                    elif 2 <= row_index <= 3: alien_sprite = Alien(str(randint(4,6)), x, y)
+                    else: alien_sprite = Alien(str(randint(1,3)), x, y)
+                    self.aliens.add(alien_sprite)
+                elif 9 <= self.level <= 10:                
+                    if row_index <= 1: alien_sprite = Alien(str(randint(7,12)), x, y)
+                    elif 2 <= row_index <= 3: alien_sprite = Alien(str(randint(4,8)), x, y)
+                    else: alien_sprite = Alien(str(randint(1,5)), x, y)
+                    self.aliens.add(alien_sprite)
+                elif 11 <= self.level <= 12:                
+                    if row_index <= 1: alien_sprite = Alien(str(randint(7,12)), x, y)
+                    elif 2 <= row_index <= 3: alien_sprite = Alien(str(randint(4,10)), x, y)
+                    else: alien_sprite = Alien(str(randint(1,8)), x, y)
+                    self.aliens.add(alien_sprite)
+                else:
+                    if row_index <= 1: alien_sprite = Alien(str(randint(10,12)), x, y)
+                    elif 2 <= row_index <= 3: alien_sprite = Alien(str(randint(6,12)), x, y)
+                    else: alien_sprite = Alien(str(randint(1,12)), x, y)
+                    self.aliens.add(alien_sprite)
+
+    def one_alien(self, x = 450, y = 947):
+        alien_sprite = Alien(str(1), x, y)
+        self.aliens.add(alien_sprite)
+
+    def alien_position_checker(self):
+        all_aliens = self.aliens.sprites()
+        for alien in all_aliens:
+            if alien.rect.right >= screen_width - 10:
+                self.alien_direction = -1
+                self.alien_move_down(1)
+            elif alien.rect.left <= 10:
+                self.alien_direction = 1
+                self.alien_move_down(1)
+
+    def alien_move_down(self, distance):
+        if self.aliens:
+            for alien in self.aliens.sprites():
+                alien.rect.y += distance
+
+    def alien_shot(self):
+        if self.aliens.sprites():
+            global power
+            random_alien = choice(self.aliens.sprites())
+            laser_sprite = Laser(random_alien.rect.center, self.level + power, screen_height)
+            self.alien_lasers.add(laser_sprite)
+
+    def extra_alien_timer(self):
+        self.extra_spawn_time -= 1
+        if self.extra_spawn_time <= 0:
+            self.extra.add(Extra(choice(['right','left']), screen_width))
+            self.extra_spawn_time = randint (1000,2000)
+
+    def collision_checks(self):
+        if self.player.sprite.lasers:
+            for laser in self.player.sprite.lasers:
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+
+                aliens_hit_check = pygame.sprite.spritecollide(laser, self.aliens, False)
+                if aliens_hit_check:
+                    for alien in aliens_hit_check:
+                        if alien.enemy_lives <= self.dmg:
+                            self.killable = True
+                        else:
+                            self.killable = False
+                            alien.enemy_lives -= self.dmg
+
+                aliens_hit = pygame.sprite.spritecollide(laser, self.aliens, self.killable)
+                if aliens_hit:
+                    for alien in aliens_hit:
+                        if self.killable == True:
+                            self.score += alien.value
+                    laser.kill()
+
+                if pygame.sprite.spritecollide(laser, self.extra, True):
+                    self.score += 100
+                    if self.level >= 6:
+                        self.choice = randint(1,4)
+                        if self.choice == 1:
+                            self.drop_shield()
+                        else: self.drop_heart()
+                    else: self.drop_heart()
+                    laser.kill()
+
+        if self.alien_lasers:
+            for laser in self.alien_lasers:
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+                if pygame.sprite.spritecollide(laser, self.player, False):
+                    laser.kill()
+                    self.lives -= 1
+                    if self.lives <= 0:
+                        self.death()
+
+        if self.aliens:
+            for alien in self.aliens:
+                pygame.sprite.spritecollide(alien, self.blocks, True)
+
+                if pygame.sprite.spritecollide(alien, self.player, False):
+                    self.death()
+
+        if self.player:
+            for player in self.player:
+                if pygame.sprite.spritecollide(player, self.heart, True):
+                    self.lives += 1
+                if pygame.sprite.spritecollide(player, self.shield, True):
+                    self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 12, y_start = 770)
+                if pygame.sprite.spritecollide(player, self.laser_speed, True):
+                    player.laser_cooldown -= 100
+                    player.speed += 1
+                    self.speed_charge -= 1
+                if pygame.sprite.spritecollide(player, self.bullet, True):
+                    player.laser_bullet += 1
+                    self.bullet_charge -= 1
+                if pygame.sprite.spritecollide(player, self.damage, True):
+                    self.dmg += 1
+
+    def display_lives(self):
+            lives_surf = self.font.render(f'{self.lives - 1}x', False, 'white')
+            screen.blit(lives_surf,(565,25))
+            screen.blit(self.live_icon,(625,10))
+
+    def display_level(self):
+        level_surf = self.font.render('Endless', False, 'white')
+        level_rect = level_surf.get_rect(topleft = (350,25))
+        screen.blit(level_surf, level_rect)
+
+    def display_score(self):
+        score_surf = self.font.render(f'Score:{self.score}', False, 'white')
+        score_rect = score_surf.get_rect(topleft = (25,25))
+        screen.blit(score_surf, score_rect)
+
+    def drop_heart(self):
+        heart_sprite = Heart((screen_width/2, 100), 4, screen_height)
+        self.heart.add(heart_sprite)
+
+    def drop_shield(self):
+        shield_sprite = Shield((screen_width/2, 100), 4, screen_height)
+        self.shield.add(shield_sprite)
+
+    def drop_perks(self):
+        if self.speed_charge >= 1:
+            speed_sprite = Speed((screen_width/2 + screen_width/4 ,200), 6, screen_height)
+            self.laser_speed.add(speed_sprite)
+        else: pass
+        if self.bullet_charge >= 1:
+            bullet_sprite = Bullet((screen_width/4, 200), 6, screen_height)
+            self.bullet.add(bullet_sprite)
+        else: pass
+        damage_sprite = Damage((screen_width/2, 200), 6, screen_height)
+        self.damage.add(damage_sprite)
+
+    def next_level(self):
+
+        if not self.aliens.sprites():
+            if self.level == 0:
+                self.level += 1
+                self.one_alien()
+                pygame.time.set_timer(ALIENSET,1700,loops = 1)
+            else:
+                self.level += 1
+                self.drop_perks()
+                self.one_alien()
+                pygame.time.set_timer(ALIENSET,1700,loops = 1)
+            
+    def run(self):
+        self.player.update()
+        self.alien_lasers.update()
+        self.extra.update()
+        self.heart.update()
+        self.shield.update()
+        self.laser_speed.update()
+        self.bullet.update()
+        self.damage.update()
+
+        self.aliens.update(self.alien_direction)
+        self.alien_position_checker()
+        self.extra_alien_timer()
+        self.collision_checks()
+
+        self.player.sprite.lasers.draw(screen)
+        self.player.draw(screen)
+        self.blocks.draw(screen)
+        self.aliens.draw(screen)
+        self.alien_lasers.draw(screen)
+        self.extra.draw(screen)
+        self.heart.draw(screen)
+        self.shield.draw(screen)
+        self.laser_speed.draw(screen)
+        self.bullet.draw(screen)
+        self.damage.draw(screen)
+
+        self.display_lives()
+        self.display_level()
+        self.display_score()
+        self.next_level()
+
 class Button:
     def __init__(self,text,width,height,pos):
         self.pressed = False
@@ -382,6 +645,12 @@ class ButtonGame:
                 self.pressed = True
             else:
                 if self.pressed == True:
+                    if button_6.pressed:
+                        game2.level = 0
+                        game2.lives = 6
+                        game2.dmg = 2
+                        power = 3
+                        gameplay2()
                     if button_10.pressed:
                         game.level = 0
                         game.lives = 2
@@ -548,6 +817,25 @@ def gameplay():
         pygame.display.flip()
         clock.tick(60)
 
+def gameplay2():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pause()
+            if event.type == ALIENLASER:
+                game2.alien_shot()
+            if event.type == ALIENSET:
+                game2.alien_setup(rows = 7, cols = 7)
+        screen.fill((30, 30, 30))
+        game2.run()
+        pygame.display.flip()
+        clock.tick(60)
+
 def main_menu():
     while True:
         screen.fill((30,30,30))
@@ -649,7 +937,7 @@ def credits():
         game_rect = game_surface.get_rect(center = (game_x,game_y))
         screen.blit(game_surface,game_rect)
 
-        game_text = str('8bit Feel Good Inc.')
+        game_text = str('My Own Bit LOL')
         game_surface = game_font.render(game_text,True,(255,255,255))
         game_x = int(screen_width/2)
         game_y = int(500)
@@ -747,6 +1035,7 @@ def menu():
         button_12.draw()
         button_11.draw()
         button_10.draw()
+        button_6.draw()
         button_4.draw()
 
         pygame.display.flip()
@@ -940,6 +1229,39 @@ def lose():
         pygame.display.flip()
         clock.tick(60)
 
+def lose2():
+    rul = True
+    while rul:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        screen.fill((30, 30, 30))
+
+        title_text = str('You Lose...')
+        title_surface = title_font.render(title_text,True,(255,255,255))
+        title_x = int(screen_width/2)
+        title_y = int(screen_height/3)
+        title_rect = title_surface.get_rect(center = (title_x,title_y))
+        screen.blit(title_surface,title_rect)
+
+        title_text = str(f'Your Score: {game2.score}')
+        title_surface = game_font.render(title_text,True,(255,255,255))
+        title_x = int(screen_width/2)
+        title_y = int(screen_height/2)
+        title_rect = title_surface.get_rect(center = (title_x,title_y))
+        screen.blit(title_surface,title_rect)
+
+        button_13.draw()
+
+        pygame.display.flip()
+        clock.tick(60)
+
 if __name__ == '__main__':
     pygame.init()
     screen_width = 700
@@ -950,6 +1272,7 @@ if __name__ == '__main__':
     pygame.display.set_caption('Earth Invaders')
     pygame.display.set_icon(icon)
     game = Game()
+    game2 = Game2()
 
     ALIENLASER = pygame.USEREVENT + 1
     pygame.time.set_timer(ALIENLASER, game.alien_cooldown)
@@ -975,9 +1298,10 @@ if __name__ == '__main__':
 
     button_0 = ButtonBack2('Back',screen_width/2,100,(screen_width/4,750))
 
-    button_10 = ButtonGame('Impossible',screen_width/2,100,(screen_width/4,550))
-    button_11 = ButtonGame('Hard',screen_width/2,100,(screen_width/4,400))
-    button_12 = ButtonGame('Normal',screen_width/2,100,(screen_width/4,250))
+    button_6 = ButtonGame('Endless',screen_width/2,100,(screen_width/4,575))
+    button_10 = ButtonGame('Impossible',screen_width/2,100,(screen_width/4,450))
+    button_11 = ButtonGame('Hard',screen_width/2,100,(screen_width/4,325))
+    button_12 = ButtonGame('Normal',screen_width/2,100,(screen_width/4,200))
 
     button_13 = ButtonRestart('Restart',screen_width/2,100,(screen_width/4,550))
 
